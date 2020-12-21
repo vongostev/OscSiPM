5013ec6495809105942309ff2bb73b92bb2b4f51
Pavel Gostev <73311144+vongostev@users.noreply.github.com>
2020-12-21 19:51:59 +0300

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,22 @@
+import setuptools
+
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setuptools.setup(
+    name="oscsipm-${{ secrets.PYPI_USERNAME }}", # Replace with your own username
+    version="0.1",
+    author="Pavel Gostev",
+    author_email="gostev.pavel@physics.msu.ru",
+    description="Instruments to make photocounting statistics from histograms and raw oscillograms",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/vongostev/OscSiPM",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
