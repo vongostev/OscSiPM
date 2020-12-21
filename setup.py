d53a9429698406e8f111bcf96aca2f01fa6134f2
HauptMachine\vonGostev <gostev.pavel@physics.msu.ru>
2020-12-21 20:38:02 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="oscsipm-vong",
+    name="oscsipm",
     version="0.1",
     author="Pavel Gostev",
     author_email="gostev.pavel@physics.msu.ru",
